ada9ebecf16252d2f58d6857f17ef2b12f089876
Herald Carl Avila <heavila@my.cspc.edu.ph>
2026-03-26 00:55:53 +0800

    Handle empty 'updated_at' field in session statistics display

--- admin/partials/wpragbot-admin-display.php
+++ admin/partials/wpragbot-admin-display.php
@@ -182,7 +182,13 @@ $settings = get_option('wpragbot_settings');
                     <tr>
                         <td><?php echo esc_html(substr($session['session_id'], 0, 20)); ?>...</td>
                         <td><?php echo esc_html($session['message_count']); ?></td>
-                        <td><?php echo esc_html(human_time_diff(strtotime($session['updated_at']), current_time('timestamp'))); ?> ago</td>
+                        <td><?php 
+                            if (isset($session['updated_at']) && !empty($session['updated_at'])) {
+                                echo esc_html(human_time_diff(strtotime($session['updated_at']), current_time('timestamp'))); 
+                            } else {
+                                echo 'Never'; 
+                            }
+                            ?> ago</td>
                     </tr>
                     <?php endforeach; ?>
                 </tbody>
